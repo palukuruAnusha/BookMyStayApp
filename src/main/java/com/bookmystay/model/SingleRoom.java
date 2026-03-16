dcd5a024ab2f9a298667e0880f6d8bb5b547bc75
palukuruAnusha <palukuruanusha09@gmail.com>
2026-03-16 18:30:34 +0530

    UC2: Basic room types with static availability

---
+++ src/main/java/com/bookmystay/model/SingleRoom.java
@@ -0,0 +1,11 @@
+package com.bookmystay.model;
+
+/**
+ * Concrete room type for a single occupancy room.
+ */
+public class SingleRoom extends Room {
+
+    public SingleRoom() {
+        super("Single", 1, 220, 75.0);
+    }
+}
